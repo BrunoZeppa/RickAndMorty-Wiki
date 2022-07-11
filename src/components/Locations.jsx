c54f7f4b84cd46133472e838b6e44283e27de342
brunoz <zegb94@gmail.com>
2022-07-10 20:01:06 -0500

    u

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -13,10 +13,15 @@ const Locations = () => {
         axios.get(`https://rickandmortyapi.com/api/location/${searchValue}`)
             .then(res => setPlaces(res.data));
     };
+    const noResidents = () => {
+        if(residents.length === 0)
+        return "this place seems to be with no residents"
+    }
+
     return (
         <div>
             <div className='search-container'>
-            <input type="text" value={searchValue} onChange={e => setSearchValue(e.target.value)} />
+            <input type="text" placeholder='Type 1 to 126 for a location' value={searchValue} onChange={e => setSearchValue(e.target.value)} />
             <button onClick={searchBtn}><i class="fa-solid fa-magnifying-glass"></i></button>
             </div>
             <div className='location'>
@@ -26,6 +31,7 @@ const Locations = () => {
             <p><span>dimansion: </span>{dimension}</p>
             <p><span>population: </span>{population}</p>
             </article>
+            {noResidents()}
             </div>
             <section className='residents-container'>
             {residents?.map( url =>(
